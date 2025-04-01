9f599bfb8ca6dec1ab27e11b3b94907fbabb2970
“Daniel <danieljuarez1996@gmail.com>
2025-04-01 16:07:19 -0300

    correccion de archivo tp2_1_2.c

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 // codigo a completar
 #define N 20
 
@@ -7,12 +9,14 @@ int main()
 
     int i;
     int rand();
+    srand(time(NULL));
     double vt[N];
     double *p;
+    p=&vt[0];
     for (i = 0; i < N; i++)
     {
         *p = 1 + rand() % 100;
-        printf("%.2f - ",*p);
+        printf("%.2f - ",*p++);
     }
     return 0;
 }
